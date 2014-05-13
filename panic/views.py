3c63d1a327d9c8267a016e96bf460020dfe977db
Kirby <kirby.k6@gmail.com>
2014-05-13 11:24:28 -0500

    Test killswitch

--- panic/views.py
+++ panic/views.py
@@ -8,8 +8,14 @@ import win32gui, win32api, win32con
 def index(request):
   return render(request, 'panic/index.html')
 
+def exit_callback(hwnd):
+  win32api.TerminateProcess(hwnd, 0)
+  win32api.CloseHandle(hwnd)
+  return true
+
 def panic(request):
   win32gui.SystemParametersInfo(win32con.SPI_SETFOREGROUNDLOCKTIMEOUT, 0, win32con.SPIF_SENDWININICHANGE | win32con.SPIF_UPDATEINIFILE)
+  win32gui.EnumWindows(exit_callback)
   window = win32api.ShellExecute(0, 'open', 'chrome.exe', '--user-data-dir=$(mktemp -d) --kiosk --start-fullscreen --new-window http://dashboard.txssc.com', '', 1)
 
   return HttpResponseRedirect(reverse('index'))
