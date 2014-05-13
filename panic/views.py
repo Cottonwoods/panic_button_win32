fa710a7b184528f0da2ea3d5387ccd183d5145ce
Kirby <kirby.k6@gmail.com>
2014-05-13 12:10:12 -0500

    Test killswitch

--- panic/views.py
+++ panic/views.py
@@ -4,16 +4,16 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 import win32gui, win32api, win32con
+from win32com.client import GetObject
 
 def index(request):
   return render(request, 'panic/index.html')
 
-def exit_callback(hwnd):
-  win32api.TerminateProcess(hwnd, 0)
-  win32api.CloseHandle(hwnd)
-  return true
-
 def panic(request):
+  def exit_callback(hwnd):
+    win32api.TerminateProcess(hwnd, 0)
+    win32api.CloseHandle(hwnd)
+    return True
   win32gui.SystemParametersInfo(win32con.SPI_SETFOREGROUNDLOCKTIMEOUT, 0, win32con.SPIF_SENDWININICHANGE | win32con.SPIF_UPDATEINIFILE)
   win32gui.EnumWindows(exit_callback)
   window = win32api.ShellExecute(0, 'open', 'chrome.exe', '--user-data-dir=$(mktemp -d) --kiosk --start-fullscreen --new-window http://dashboard.txssc.com', '', 1)
